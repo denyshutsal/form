abd605f208992f1825209981d304222d59c701fc
Denys Hutsal <denyshutsal@gmail.com>
2023-01-27 16:52:37 +0100

    feat: add SignUpForm component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
+import SignUpForm from "./components/sign-up-form/SignUpForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SignUpForm />
+    </div>
+  );
 }
 
 export default App;
